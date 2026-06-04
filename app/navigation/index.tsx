ef5b88ccf2bf21247c323ee042737b76b3321be4
Natesh kumar <nateshkumar1406@gmail.com>
2026-06-04 12:58:16 +0530

    Latest Code

--- app/navigation/index.tsx
+++ app/navigation/index.tsx
@@ -59,7 +59,7 @@ import QuickAddWatchlist from "app/screens/quickAddWashlist/quickAddWatchlist";
 import QuickAddWatchlistForm from "app/screens/quickAddWashlist/quickAddWatchlistForm";
 import Ionicons from "react-native-vector-icons/Ionicons";
 import Faq from "app/screens/faq/Faq";
-
+import ChatSupport from "app/screens/chatSupport/ChatSupport";
 
 export default function Navigation({
 
@@ -303,6 +303,16 @@ const RootNavigator = () => {
         }}
       />
 
+      <Stack.Screen name="ChatSupport"
+        component={ChatSupport}
+        options={{
+          animation: 'slide_from_right',
+          title: 'Chat Support',
+          headerShown: false,
+        }}
+      />
+
+
 
     </Stack.Navigator>
   );
@@ -428,6 +438,20 @@ const DrawerNavigator = () => {
           ),
         }}
       />
+
+      {/* Help & Support */}
+      <Drawer.Screen
+        name="HelpAndSupportDrawer"
+        component={ChatSupport}
+        options={{
+          title: "Help & Support",
+          headerShown: false,
+          drawerIcon: ({ color, size }) => (
+            <Ionicons name="headset" size={size} color={color} />
+          ),
+        }}
+      />
+
 
     </Drawer.Navigator>
 
